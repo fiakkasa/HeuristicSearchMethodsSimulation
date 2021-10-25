eb70f264d570553d64182801ee65a2eb671e23a7
fiakkasa <fiakkasa@live.com>
2021-10-25 18:37:52 +0300

    Add algo drop down Reset data when no algo selected Add progress bar

---
+++ HeuristicSearchMethodsSimulation/Enums/TravelingSalesManAlgorithms.cs
@@ -0,0 +1,12 @@
+﻿namespace HeuristicSearchMethodsSimulation.Enums
+{
+    public enum TravelingSalesManAlgorithms
+    {
+        None = 0,
+        Partial_Random = 1,
+        Exhaustive = 2,
+        Partial_Improving = 3,
+        Guided_Direct = 4,
+        Evolutionary = 5
+    }
+}
